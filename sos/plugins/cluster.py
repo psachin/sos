4cdabeb1083021210dd6e8fa0014ce46a3cff664
Shane Bradley <sbradley@redhat.com>
2015-06-25 11:49:18 -0400

    [cluster.py] The plugin was split into multiple different plugins
    The cluster.py plugin was split into multiple different plugins
    because it was composed of multiple utilities: dlm, gfs2, pacemaker,
    and cluster. The corosync commands were removed from the cluster
    plugin since a corosync plugin already exists to capture that
    information. The cluster plugin is now the main plugin for
    gathering information about "cman" plugins.
    
    Signed-off-by: Shane Bradley <sbradley@redhat.com>

--- sos/plugins/cluster.py
+++ sos/plugins/cluster.py
@@ -14,9 +14,7 @@
 
 from sos.plugins import Plugin, RedHatPlugin
 import re
-import os.path
 from glob import glob
-from datetime import datetime, timedelta
 
 
 class Cluster(Plugin, RedHatPlugin):
@@ -26,22 +24,13 @@ class Cluster(Plugin, RedHatPlugin):
     plugin_name = 'cluster'
     profiles = ('cluster',)
 
-    option_list = [
-        ("gfs2lockdump", 'gather output of gfs2 lockdumps', 'slow', False),
-        ("crm_from", 'specify the start time for crm_report', 'fast', False),
-        ('lockdump', 'gather dlm lockdumps', 'slow', False),
-        ('crm_scrub', 'enable password scrubbing for crm_report', '', True),
-    ]
-
     packages = [
         "luci",
         "ricci",
-        "corosync",
         "openais",
         "cman",
         "clusterlib",
         "fence-agents",
-        "pacemaker"
     ]
 
     files = ["/etc/cluster/cluster.conf"]
@@ -54,8 +43,6 @@ class Cluster(Plugin, RedHatPlugin):
         self.add_copy_spec([
             "/etc/cluster.conf",
             "/etc/cluster",
-            "/etc/sysconfig/dlm",
-            "/etc/sysconfig/pacemaker",
             "/etc/sysconfig/cluster",
             "/etc/sysconfig/cman",
             "/etc/fence_virt.conf",
@@ -64,89 +51,20 @@ class Cluster(Plugin, RedHatPlugin):
             "/var/lib/luci/etc",
             "/var/log/cluster",
             "/var/log/luci",
-            "/sys/fs/gfs2/*/withdraw"
         ])
-
-        if self.get_option('gfs2lockdump'):
-            if self._mount_debug():
-                self.add_copy_spec(["/sys/kernel/debug/gfs2/*"])
-
-        if self.get_option('lockdump'):
-            self.do_lockdump()
 
         self.add_cmd_output([
             "rg_test test /etc/cluster/cluster.conf",
             "fence_tool ls -n",
-            "gfs_control ls -n",
-            "dlm_tool log_plock",
             "clustat",
             "group_tool dump",
             "cman_tool services",
             "cman_tool nodes",
             "cman_tool status",
             "ccs_tool lsnode",
-            "corosync-quorumtool -l",
-            "corosync-quorumtool -s",
-            "corosync-cpgtool",
-            "corosync-objctl",
-            "gfs_control ls -n",
-            "gfs_control dump",
             "fence_tool dump",
-            "dlm_tool dump",
-            "dlm_tool ls -n",
-            "mkqdisk -L",
-            "pcs config",
-            "pcs status",
-            "pcs property list --all"
+            "mkqdisk -L"
         ])
-
-        # crm_report needs to be given a --from "YYYY-MM-DD HH:MM:SS" start
-        # time in order to collect data.
-        crm_from = (datetime.today() -
-                    timedelta(hours=72)).strftime("%Y-%m-%d %H:%m:%S")
-        if self.get_option('crm_from') is not False:
-            if re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',
-                        str(self.get_option('crm_from'))):
-                crm_from = self.get_option('crm_from')
-            else:
-                self._log_error(
-                    "crm_from parameter '%s' is not a valid date: using "
-                    "default" % self.get_option('crm_from'))
-
-        crm_dest = self.get_cmd_output_path(name='crm_report', make=False)
-        crm_scrub = '-p "passw.*"'
-        if not self.get_option("crm_scrub"):
-            crm_scrub = ''
-            self._log_warn("scrubbing of crm passwords has been disabled:")
-            self._log_warn("data collected by crm_report may contain"
-                           " sensitive values.")
-        self.add_cmd_output('crm_report %s -S -d --dest %s --from "%s"'
-                            % (crm_scrub, crm_dest, crm_from))
-
-    def do_lockdump(self):
-        if self._mount_debug():
-            dlm_tool = "dlm_tool ls"
-            result = self.call_ext_prog(dlm_tool)
-            if result['status'] != 0:
-                return
-
-            lock_exp = r'^name\s+([^\s]+)$'
-            lock_re = re.compile(lock_exp, re.MULTILINE)
-            for lockspace in lock_re.findall(result['output']):
-                self.add_cmd_output(
-                    "dlm_tool lockdebug -svw '%s'" % lockspace,
-                    suggest_filename="dlm_locks_%s" % lockspace
-                )
-
-    def _mount_debug(self):
-        if not os.path.ismount(self.debugfs_path):
-            self._debugfs_cleanup = True
-            r = self.call_ext_prog("mount -t debugfs debugfs %s"
-                                   % self.debugfs_path)
-            if r['status'] != 0:
-                self._log_error("debugfs not mounted and mount attempt failed")
-                self._debugfs_cleanup = False
-        return os.path.ismount(self.debugfs_path)
 
     def postproc(self):
         for cluster_conf in glob("/etc/cluster/cluster.conf*"):
@@ -161,17 +79,6 @@ class Cluster(Plugin, RedHatPlugin):
             r"(.*secret\s*=\s*)\S+",
             r"\1******"
         )
-
-        self.do_cmd_output_sub(
-            "corosync-objctl",
-            r"(.*fence.*\.passwd=)(.*)",
-            r"\1******"
-        )
-        if self._debugfs_cleanup and os.path.ismount(self.debugfs_path):
-            r = self.call_ext_prog("umount %s" % self.debugfs_path)
-            if r['status'] != 0:
-                self._log_error("could not unmount %s" % self.debugfs_path)
-
         return
 
 # vim: et ts=4 sw=4
